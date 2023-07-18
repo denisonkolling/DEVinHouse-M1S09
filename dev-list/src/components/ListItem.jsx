6e2be6cd6a6ba33c7f3b9dfcbe6e9ac8d0229045
Denison Kolling <denisonkolling@gmail.com>
2023-07-18 11:07:25 -0300

    altera funcao finalizar tarefa

--- dev-list/src/components/ListItem.jsx
+++ dev-list/src/components/ListItem.jsx
@@ -2,22 +2,23 @@ import React, { useState } from 'react';
 
 const ListItem = ({ list, removeTask }) => {
 
-	const [done, setDone] = useState();
-
-	const endTask = (id) => {
-    const index = list.findIndex((task) => task.id === id);
-		setDone((list[index].done = !done));
+	
+	const [done, setDone] = useState(list.done);
+	
+	const endTask = (index) => {
+			setDone(list[index].done = !list[index].done);
+			console.log(list)
 	};
 
 	return (
 		<div>
 			<h1>Your Tasks List</h1>
-			{list.map((task) => (
+			{list.map((task, index) => (
 				<div className="task-list" key={task.id}>
 					<h3>{task.text}</h3>
 					<p>{task.done == true ? 'Completed!' : 'Not done!'}</p>
 					<div>
-						<button onClick={() => endTask(task.id)}>Finish</button>
+						<button onClick={() => endTask(index)}>Finish</button>
 						<button onClick={() => removeTask(task.id)}>Remove</button>
 					</div>
 				</div>
